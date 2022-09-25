c512036f403f8c4d8fd69377023784142438a177
Ezequiel Machado Ferreira <ezequielmmf18@gmail.com>
2022-09-25 17:27:22 -0300

    autom1.py - fazendo login

--- autom1.py
+++ autom1.py
@@ -1,6 +1,18 @@
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
+
+navegador.get('https://servicedesk.ancora.com.br:7443/login.jsp?os_destination=%2Fprojects%2FTSD%2Fqueues%2Fcustom%2F1&permissionViolation=true')
+time.sleep(2)
+navegador.find_element('xpath', '//*[@id="login-form-username"]').send_keys('ezequiel.ferreira')
+navegador.find_element('xpath', '//*[@id="login-form-password"]').send_keys('Mentalidad3')
+navegador.find_element('xpath', '//*[@id="login-form-submit"]').click()
+time.sleep(4)
+navegador.find_element(By.LINK_TEXT, value='Verificar Backups').click()
+
+time.sleep(20)
